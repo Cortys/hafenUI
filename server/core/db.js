616ccb9296a338a3eb617ab07e37e0772cfbbc9d
Clemens Damke <admin@cortys.de>
2014-01-30 20:18:44 +0100

    Vectorized images + DB bug fixes

--- server/core/db.js
+++ server/core/db.js
@@ -3,8 +3,23 @@ var db = {
 	connectionData: require("./settings.js").dbData,
 	connection: null,
 	init: function() {
+	    var t = this;
 		this.connection = this.mysql.createConnection(this.connectionData);
-		console.log("> Database connection established with state: "+this.connection.state);
+		this.connection.connect(function(err) {
+            console.log("> Database connection established with state: "+t.connection.state);
+            if(err) {
+                console.log("> Retrying to connect in 2000 ms", err);
+                setTimeout(function() {
+                    t.init();
+                }, 2000);
+            }
+        });
+        this.connection.on('error', function(err) {
+            if(err.code === "PROTOCOL_CONNECTION_LOST")
+                t.init();
+            else
+                console.log("> Unsolvable DB error", err);
+		});
 	}
 };
 db.init();
