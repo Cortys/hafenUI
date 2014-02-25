6a68c6f35f3dcfb2ae682cd7b46f40270ed14740
Marvin <github@gfield.de>
2014-02-25 18:54:32 +0100

    fixed java bt client

--- java/NXTProtocol.java
+++ java/NXTProtocol.java
@@ -21,6 +21,7 @@ public class NXTProtocol extends Thread {
 
     public NXTProtocol(String pPath) {
         System.out.println(pPath);
+        new FWriter(pPath + "/send.txt", false);
         this.reader = new FReader(pPath + "/send.txt");
         this.writer = new FWriter(pPath + "/receive.txt", true);
         this.clients = new Hashtable<String, NxtBluetoothConnection>();
@@ -47,20 +48,19 @@ public class NXTProtocol extends Thread {
     private String interpret(String cmd) {
         String response = "Error";
         String[] splitted = cmd.split(":");
-       
 
-        //if(!splitted[0].equals("k")) {
+        // if(!splitted[0].equals("k")) {
         switch (splitted[1]) {
         case "c": // connect
             if (getClient(splitted[0]) == null) {
                 addClient(splitted[0], new NxtBluetoothConnection(splitted[2], this.writer, splitted[0]));
                 getClient(splitted[0]).connect(); // TODO server sagen, ob
-                          try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        }                        // connect geklappt hat
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } // connect geklappt hat
                 getClient(splitted[0]).start();
             }
             break;
@@ -83,7 +83,7 @@ public class NXTProtocol extends Thread {
         case "k": // drop clients hashtable
             this.clients = new Hashtable<String, NxtBluetoothConnection>();
             break;
-        
+
         }
         return response;
 
@@ -92,17 +92,15 @@ public class NXTProtocol extends Thread {
     public void checkFile() {
         String s = reader.readAndDelete();
         if (s != null)
-                interpret(s);
+            interpret(s);
     }
 
     public static void main(String[] args) {
-       NXTProtocol np = new NXTProtocol("C:/Users/ag-ddi/Documents/GitHub/HafenUI/bluetooth");
-      // NXTProtocol np = new NXTProtocol("./bluetooth");
+        //NXTProtocol np = new NXTProtocol("C:/Users/ag-ddi/Documents/GitHub/HafenUI/bluetooth");
+        NXTProtocol np = new NXTProtocol("./bluetooth");
 
-       
-       
         while (true) {
-            
+
             try {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
@@ -111,38 +109,20 @@ public class NXTProtocol extends Thread {
             }
             np.checkFile();
         }
-       
-       
-       
 
         /*
-        NxtBluetoothConnection tempConn = new
-        NxtBluetoothConnection("001653016143", np.writer, "0.0.0.0");
-        tempConn.connect();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        for (byte b : np.stringToByteArray("i")) {
-            tempConn.send(b);
-        }
-         tempConn.send((byte) 1);
-         tempConn.send((byte) 1);
-         
-         try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-         
-         for (byte b : np.stringToByteArray("r")) {
-             tempConn.send(b);
-         }
-          tempConn.send((byte) 1);
-          tempConn.send((byte) 1);
-     */
+         * NxtBluetoothConnection tempConn = new
+         * NxtBluetoothConnection("001653016143", np.writer, "0.0.0.0");
+         * tempConn.connect(); try { Thread.sleep(1000); } catch
+         * (InterruptedException e) { // TODO Auto-generated catch block
+         * e.printStackTrace(); } for (byte b : np.stringToByteArray("i")) {
+         * tempConn.send(b); } tempConn.send((byte) 1); tempConn.send((byte) 1);
+         * 
+         * try { Thread.sleep(10000); } catch (InterruptedException e) { // TODO
+         * Auto-generated catch block e.printStackTrace(); }
+         * 
+         * for (byte b : np.stringToByteArray("r")) { tempConn.send(b); }
+         * tempConn.send((byte) 1); tempConn.send((byte) 1);
+         */
     }
 }
