a5f917be23f2756636414ec45da750f7e6752bc9
Marvin <github@gfield.de>
2014-02-25 18:36:21 +0100

    Updated java bluetooth client! 1, 2, WHOOP WHOPP
    Added the runnable jar

--- java/NXTProtocol.java
+++ java/NXTProtocol.java
@@ -1,4 +1,7 @@
 import java.util.Hashtable;
+import java.util.Scanner;
+
+import org.jfree.util.WaitingImageObserver;
 
 import sun.io.CharToByteASCII;
 
@@ -17,6 +20,7 @@ public class NXTProtocol extends Thread {
     // ____________________________CONSTRUCTOR____________________________
 
     public NXTProtocol(String pPath) {
+        System.out.println(pPath);
         this.reader = new FReader(pPath + "/send.txt");
         this.writer = new FWriter(pPath + "/receive.txt", true);
         this.clients = new Hashtable<String, NxtBluetoothConnection>();
@@ -24,42 +28,45 @@ public class NXTProtocol extends Thread {
 
     // ____________________________METHODS____________________________
 
-    
     private void addClient(String ip, NxtBluetoothConnection conn) {
         this.clients.put(ip, conn);
     }
 
-    
     private void deleteClient(String ip) {
         this.clients.remove(ip);
     }
 
-    
     private NxtBluetoothConnection getClient(String ip) {
         return this.clients.get(ip);
     }
 
-    
     private byte[] stringToByteArray(String s) {
         return s.getBytes();
     }
 
-    
     private String interpret(String cmd) {
         String response = "Error";
         String[] splitted = cmd.split(":");
-//        for(String s : splitted)
-//            System.out.println(s);
+       
 
+        //if(!splitted[0].equals("k")) {
         switch (splitted[1]) {
         case "c": // connect
-           if (getClient(splitted[0]) == null) {
-                addClient(splitted[0], new NxtBluetoothConnection(splitted[2], this.writer, splitted[0] ));
-                
+            if (getClient(splitted[0]) == null) {
+                addClient(splitted[0], new NxtBluetoothConnection(splitted[2], this.writer, splitted[0]));
+                getClient(splitted[0]).connect(); // TODO server sagen, ob
+                          try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }                        // connect geklappt hat
+                getClient(splitted[0]).start();
             }
             break;
         case "d": // disconnect
             if (getClient(splitted[0]) != null) {
+                getClient(splitted[0]).disconnect();
                 this.deleteClient(splitted[1]);
             }
             break;
@@ -69,44 +76,73 @@ public class NXTProtocol extends Thread {
                 for (byte b : stringToByteArray(splitted[2])) {
                     tempConn.send(b);
                 }
-                tempConn.send((byte) 0);
-                tempConn.send((byte) 0);
-           }
+                tempConn.send((byte) 1);
+                tempConn.send((byte) 1);
+            }
             break;
+        case "k": // drop clients hashtable
+            this.clients = new Hashtable<String, NxtBluetoothConnection>();
+            break;
+        
         }
         return response;
 
     }
 
-    
-    private String receive() {
-        
-    }
-    
-    
-    public void checkFile(){
-        interpret(reader.readAndDelete());
+    public void checkFile() {
+        String s = reader.readAndDelete();
+        if (s != null)
+                interpret(s);
     }
 
     public static void main(String[] args) {
-       NXTProtocol np = new NXTProtocol("./send.txt");
-//        while(true)
-//            np.checkFile();
+       NXTProtocol np = new NXTProtocol("C:/Users/ag-ddi/Documents/GitHub/HafenUI/bluetooth");
+      // NXTProtocol np = new NXTProtocol("./bluetooth");
+
        
-//        np.interpret("test:c:001653016143");
-//        np.interpret("test:s:r");
-//        
-        
        
-       NxtBluetoothConnection tempConn = new NxtBluetoothConnection("0.0.0.0", np.writer, "001653016143");
-       for (byte b : np.stringToByteArray("r")) {
-           tempConn.send(b);
-       }
-       tempConn.send((byte) 1);
-       tempConn.send((byte) 1);
+        while (true) {
+            
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            np.checkFile();
+        }
        
-       while(true) {
-           System.out.println(tempConn.receive());
-       }
+       
+       
+
+        /*
+        NxtBluetoothConnection tempConn = new
+        NxtBluetoothConnection("001653016143", np.writer, "0.0.0.0");
+        tempConn.connect();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        for (byte b : np.stringToByteArray("i")) {
+            tempConn.send(b);
+        }
+         tempConn.send((byte) 1);
+         tempConn.send((byte) 1);
+         
+         try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+         
+         for (byte b : np.stringToByteArray("r")) {
+             tempConn.send(b);
+         }
+          tempConn.send((byte) 1);
+          tempConn.send((byte) 1);
+     */
     }
 }
--- java/NxtBluetoothConnection.java
+++ java/NxtBluetoothConnection.java
@@ -1,11 +1,12 @@
 import lejos.pc.comm.*;
+
 import java.io.*;
 
-
 /**
- * Diese Klasse dient dazu, die Kommunikation für die Laptop-Datenbank mit dem Fahrzeug bereitzustellen.
+ * Diese Klasse dient dazu, die Kommunikation für die Laptop-Datenbank mit dem
+ * Fahrzeug bereitzustellen.
  */
-public class NxtBluetoothConnection extends Thread{
+public class NxtBluetoothConnection extends Thread {
 
     public DataOutputStream dos;
     public DataInputStream dis;
@@ -13,61 +14,84 @@ public class NxtBluetoothConnection extends Thread{
     private boolean connected;
     private String ip;
 
-   // private final String NXT_NAME;
+    // private final String NXT_NAME;
     private final String NXT_ADDRESS;
 
-    public NxtBluetoothConnection(String address, FWriter fw, String ip){
-    	//TODO formatieren
+    public NxtBluetoothConnection(String address, FWriter fw, String ip) {
+        // TODO formatieren
         NXT_ADDRESS = address;
         fileWriter = fw;
         this.ip = ip;
-        connect();
+
+        // this.start();
     }
 
     /**
-     * Diese Methode baut eine Verbindung mit dem vorgegebenen NXT auf
-     * und weist die DataInupt- und DataOutputStreams zu.
+     * Diese Methode baut eine Verbindung mit dem vorgegebenen NXT auf und weist
+     * die DataInupt- und DataOutputStreams zu.
      */
-    public void connect(){
-    	NXTConnector conn = new NXTConnector();
+    public boolean connect() {
+        NXTConnector conn = new NXTConnector();
         connected = false;
-    
-        while(!connected){
+
+        while (!connected) {
             connected = conn.connectTo(null, NXT_ADDRESS, NXTCommFactory.BLUETOOTH);
         }
-        System.out.println("CONNECTED");
-        if (!connected) 
-        {System.err.println("Failed to connect to NXT");}
+        if (connected) {
+            //this.start();
 
-        dos = new DataOutputStream(conn.getOutputStream());
-        dis = new DataInputStream(conn.getInputStream()); 
+            System.out.println("CONNECTED");
+            
+            fileWriter.writeToFile(ip + ":c");
+            
+            System.out.println("wrote2");
+
+            dos = new DataOutputStream(conn.getOutputStream());
+            dis = new DataInputStream(conn.getInputStream());
+
+        } else {
+            System.err.println("Failed to connect to NXT");
+            fileWriter.writeToFile(ip + ":d");
+        }
+        return connected;
+
     }
 
+    public void disconnect() {
+        try {
+            this.dos.close();
+        } catch (IOException e) {
+            System.out.println("couldnt close dos");
+            e.printStackTrace();
+        }
+        try {
+            this.dis.close();
+        } catch (IOException e) {
+            System.out.println("couldnt close dis");
+            e.printStackTrace();
+        }
+    }
 
-
-
-     /**
+    /**
      * Diese Methode sendet ein Byte.
-     *
-     * @param i - Das zu sendende Byte
+     * 
+     * @param i
+     *            - Das zu sendende Byte
      */
     public void send(byte i) {
-        try{
+        try {
             dos.writeByte(i);
             dos.flush();
             System.out.println("sent");
-        }
-        catch (IOException ioe) {
+        } catch (IOException ioe) {
             System.out.println("IO Exception sending int:");
             System.out.println(ioe.getMessage());
         }
     }
 
-
-
     /**
      * Diese Methode liest ein empfangenes Byte.
-     *
+     * 
      * @return - Das empfangene Byte
      */
     public byte receive() {
@@ -75,28 +99,26 @@ public class NxtBluetoothConnection extends Thread{
         try {
             i = dis.readByte();
             System.out.println("received");
-        }
-        catch (IOException ioe) {
+        } catch (IOException ioe) {
             System.out.println("IO Exception reading bytes:");
             System.out.println(ioe.getMessage());
         }
-        
+
         return i;
     }
-    
+
     public void run() {
-        while(connected) {
+        while (connected) {
             String msg = "";
             char r = (char) receive();
             if (r == 0) {
-                //TODO diese Methode noch aufrufen
+                // TODO diese Methode noch aufrufen
                 fileWriter.writeToFile(this.ip + ":r:" + msg);
-            }else {
-                msg += r; 
+            } else {
+                msg += r;
             }
-                
-           
+
         }
-            
+
     }
 }
